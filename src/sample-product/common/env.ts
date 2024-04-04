59a23632b93aec4b9a53ba46a041c66fc7206395
gonkunkun <yuta.sasaki.ipu@gmail.com>
2024-04-05 00:07:56 +0900

    fix load env

--- src/sample-product/common/env.ts
+++ src/sample-product/common/env.ts
@@ -1,12 +1,8 @@
-// @ts-ignore
-import { parse } from "k6/x/dotenv"
 import { toBoolean } from './common'
 
-// eslint-disable-next-line @typescript-eslint/naming-convention
-const _ENV = parse(open("../.env"))
-export const DEBUG =toBoolean(_ENV.DEBUG) || false
-export const ENV = _ENV.ENV || 'local'
-export const CONFIG_PATH = _ENV.CONFIG_PATH || ENV.CONFIG_PATH || '../src/sample-product/configs/smoke.json'
-export const REDIS_ENDPOINT = _ENV.REDIS_ENDPOINT || 'redis://localhost:6379'
-export const AMOUNT_OF_INDEX_SIZE_FOR_TEST_DATA = _ENV.AMOUNT_OF_INDEX_SIZE_FOR_TEST_DATA || 10000
-export const SAMPLE_PRODUCT_ENDPOINT = _ENV.SAMPLE_PRODUCT_ENDPOINT || 'localhost'
+export const DEBUG =toBoolean(__ENV.DEBUG) || false
+export const ENV = __ENV.ENV || 'local'
+export const CONFIG_PATH = __ENV.CONFIG_PATH || '../src/sample-product/configs/smoke.json'
+export const REDIS_ENDPOINT = __ENV.REDIS_ENDPOINT || 'redis://localhost:6379'
+export const AMOUNT_OF_INDEX_SIZE_FOR_TEST_DATA = __ENV.AMOUNT_OF_INDEX_SIZE_FOR_TEST_DATA || 10000
+export const SAMPLE_PRODUCT_ENDPOINT = __ENV.SAMPLE_PRODUCT_ENDPOINT || 'localhost'
